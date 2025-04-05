66844466d30ae8ba345d91ba1917d8675379ed10
bovimacoco <www.clintonboniface@gmail.com>
2025-04-05 12:16:26 +0100

    T386712 Suppress SQLAlchemy Database track warning message bug: T386712
    Change-Id: Iaafcb29b00569bd256465cf74e997585b9e2d089

--- run.py
+++ run.py
@@ -7,6 +7,9 @@ print(f"Current working directory: {os.getcwd()}")
 # Create the Flask app
 app = create_app()
 
+# Suppress SQLAlchemy track modifications warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)  # Run the app in debug mode
